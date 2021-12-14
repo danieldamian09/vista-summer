90d1b695677cdf113c29c46749d7ac7f87688391
danieldamian09 <humbertorivero9@gmail.com>
2021-12-14 12:16:57 -0300

    actualizando condicional vistas

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var pasoActual = 1;
+
 var app = document.querySelector('#app');
+
 var capsulaUno = document.querySelector('#capsulaUno');
 var capsulaDos = document.querySelector('#capsulaDos');
 var capsulaTres = document.querySelector('#capsulaTres');
 var capsulaCuatro = document.querySelector('#capsulaCuatro');
 var capsulaCinco = document.querySelector('#capsulaCinco');
+
+var intro = document.querySelector('.content_intro')
 var containerUno = document.querySelector('.uno')
 var containerDos = document.querySelector('.dos')
 var containerTres = document.querySelector('.tres')
@@ -12,10 +16,24 @@ var containerCuatro = document.querySelector('.cuatro')
 var containerCinco = document.querySelector('.cinco')
 
 
-
-if (pasoActual === 0) {
-  app.style.display = "none"
+if(pasoActual === 1){
+  intro.style.display = 'block'
+}else if(pasoActual === 2){
+  containerUno.style.display = 'block'
+}else if(pasoActual === 3){
+  containerDos.style.display = 'block'
+}else if(pasoActual === 4){
+  containerTres.style.display = 'block'
+}else if(pasoActual === 5){
+  containerCuatro.style.display = 'block'
+}else if(pasoActual === 6){
+  containerCinco.style.display = 'block'
 }
+
+
+// if (pasoActual === 0) {
+//   app.style.display = "none"
+// }
 
 // for(const avanza of avanzar){
 //   avanza.addEventListener('click', function(e){
@@ -54,49 +72,41 @@ if (pasoActual === 0) {
 
 capsulaUno.addEventListener('click', function () {
   containerUno.style.display = 'block';
-  console.log("Estoy en la capsula uno")
+  pasoActual = 1;
+  console.log(pasoActual)
   const audioDos = new Audio('./assets/audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 2.mp4')
-  let banderaUno = 0;
-  if (banderaUno === 0) {
-    audioDos.play()
-    banderaUno++;
-  }else{
-    audioDos.pause()
-  }
+  audioDos.play()
 
 })
 
 capsulaDos.addEventListener('click', function () {
   containerDos.style.display = 'block';
-  console.log("Estoy en la capsula dos")
+  pasoActual = 2;
+  console.log(pasoActual)
   const audioTres = new Audio('./assets/audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 3.mp4')
-  let banderaDos = 0;
-  if (banderaDos === 0) {
-    audioTres.play()
-    banderaDos++;
-    console.log(banderaDos)
-  }else if(banderaDos === 1){
-    audioTres.pause()
-  }
+  audioTres.play()
 })
 
 capsulaTres.addEventListener('click', function () {
   containerTres.style.display = 'block';
-  console.log("Estoy en la capsula tres")
+  pasoActual = 3;
+  console.log(pasoActual)
   const audioCuatro = new Audio('./assets/audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 4.mp4')
   audioCuatro.play()
 })
 
 capsulaCuatro.addEventListener('click', function () {
   containerCuatro.style.display = 'block';
-  console.log("Estoy en la capsula cuatro")
+  pasoActual = 4;
+  console.log(pasoActual)
   const audioCinco = new Audio('./assets/audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 5.mp4')
   audioCinco.play()
 })
 
 capsulaCinco.addEventListener('click', function () {
   containerCinco.style.display = 'block';
-  console.log("Estoy en la capsula cinco")
+  pasoActual = 5;
+  console.log(pasoActual)
   const audioSeis = new Audio('./assets/audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 6.mp4')
   audioSeis.play()
 })
