2556e5c43bc98bec76ff0da6266fc4f2346df3a4
danieldamian09 <humbertorivero9@gmail.com>
2021-12-14 08:51:31 -0300

    audios

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var containerCinco = document.querySelector('.cinco')
 
 
 
-if(pasoActual === 0){
+if (pasoActual === 0) {
   app.style.display = "none"
 }
 
@@ -25,31 +25,65 @@ if(pasoActual === 0){
 //   })
 // }
 
-if (app.requestFullscreen) {
-  app.requestFullscreen();
-}
 
-capsulaUno.addEventListener('click', function(){
+
+// Obligar rotacion de la pantalla
+// jQuery(window).bind('orientationchange', function (e) {
+//   switch (window.orientation) {
+//     case 0:
+//       $('.turnDeviceNotification').css('display', 'none');
+//       // The device is in portrait mode now
+//       break;
+
+//     case 180:
+//       $('.turnDeviceNotification').css('display', 'none');
+//       // The device is in portrait mode now
+//       break;
+
+//     case 90:
+//       // The device is in landscape now
+//       $('.turnDeviceNotification').css('display', 'block');
+//       break;
+
+//     case -90:
+//       // The device is in landscape now
+//       $('.turnDeviceNotification').css('display', 'block');
+//       break;
+//   }
+// });
+
+capsulaUno.addEventListener('click', function () {
   containerUno.style.display = 'block';
   console.log("Estoy en la capsula uno")
+  const audioDos = new Audio('./audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 2.mp4')
+  audioDos.play()
+
 })
 
-capsulaDos.addEventListener('click', function(){
+capsulaDos.addEventListener('click', function () {
   containerDos.style.display = 'block';
   console.log("Estoy en la capsula dos")
+  const audioTres = new Audio('./audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 3.mp4')
+  audioTres.play()
 })
 
-capsulaTres.addEventListener('click', function(){
+capsulaTres.addEventListener('click', function () {
   containerTres.style.display = 'block';
   console.log("Estoy en la capsula tres")
+  const audioCuatro = new Audio('./audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 4.mp4')
+  audioCuatro.play()
 })
 
-capsulaCuatro.addEventListener('click', function(){
+capsulaCuatro.addEventListener('click', function () {
   containerCuatro.style.display = 'block';
   console.log("Estoy en la capsula cuatro")
+  const audioCinco = new Audio('./audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 5.mp4')
+  audioCinco.play()
 })
 
-capsulaCinco.addEventListener('click', function(){
+capsulaCinco.addEventListener('click', function () {
   containerCinco.style.display = 'block';
   console.log("Estoy en la capsula cinco")
+  const audioSeis = new Audio('./audios/Fede Pollevik Marlboro Vista Summer Fusion Loc 6.mp4')
+  audioSeis.play()
 })
